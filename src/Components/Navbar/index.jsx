354956fc816bf4774fe9d81c4a6fa9b3f79f179b
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-11-12 20:10:16 -0500

    navbar and some elements have been finished

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import logo from "./rithvikdineshlogo.png";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLnk,
+} from "./NavbarElements";
+
+const index = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <img src={logo} alt="Logo" />
+          </NavLogo>
+
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLnk to="/signin">Sign In</NavBtnLnk>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default index;
